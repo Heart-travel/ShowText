5afebf9b4533e06075ad702c831b901bfa8967bc
Heart-travel <zcgacbc@gmail.com>
2015-04-29 21:52:44 +0800

    Insert the TextView which can show any text

--- src/com/example/showtext/MainActivity.java
+++ src/com/example/showtext/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		textView=(TextView)findViewById(R.id.textView);
+	    textView.setText("Hello Android");
 	}
 
 	@Override
